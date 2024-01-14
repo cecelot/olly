dab11664afba263af439e161e7be827d02853d3e
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-01-14 17:45:05 -0600

    refactor: easier-to-press buttons

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -9,13 +9,19 @@ export default function Home() {
           <h2 class="font-normal text-gray-300 text-xl">
             The two-player strategy board game based on Reversi
           </h2>
-          <div class="flex-row space-x-5">
-            <button class="bg-green-400 hover:bg-green-500 transition-all text-slate-950 p-3 rounded-lg">
-              <A href="/play">New Game</A>
-            </button>
-            <button class=" bg-green-200 hover:bg-green-300 transition-all text-slate-950 p-3 rounded-lg">
-              <A href="/join">Join Game</A>
-            </button>
+          <div class="flex-row space-x-5 pt-5">
+            <A
+              href="/play"
+              class="bg-green-400 hover:bg-green-500 transition-all text-slate-950 rounded-lg p-3"
+            >
+              New Game
+            </A>
+            <A
+              href="/play"
+              class="bg-green-200 hover:bg-green-300 transition-all text-slate-950 rounded-lg p-3"
+            >
+              Join Game
+            </A>
           </div>
         </section>
       </main>
