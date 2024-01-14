80128e9aaff4236d41cbe5c039a4147f205f66d7
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-01-14 14:12:37 -0600

    refactor: unnecessary(?) `createEffect`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { For, Show, createSignal, createEffect } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 import { createWS } from "@solid-primitives/websocket";
 import { Board, Piece, Event } from "./types";
 import {
@@ -28,38 +28,36 @@ function App() {
   const [gameId, setGameId] = createSignal<string>();
   const ws = createWS("ws://0.0.0.0:3000/live");
 
-  createEffect(() => {
-    ws.addEventListener("message", (e) => {
-      const data: Event = JSON.parse(e.data);
-      const handlers = {
-        1: handleAckEvent,
-        2: handleReady,
-        3: handleGameCreate,
-        4: handleGameUpdate,
-        5: handleErrorEvent,
-      } as const;
-      console.log(data);
-      handlers[data.op](
-        ws,
-        data as any,
-        setToken,
-        setGameId,
-        setTurn,
-        board,
-        token
-      );
-    });
-    ws.send(
-      JSON.stringify({
-        op: 6,
-        d: {
-          type: "Identify",
-          username: "alaidriel",
-          password: "meow",
-        },
-      })
+  ws.addEventListener("message", (e) => {
+    const data: Event = JSON.parse(e.data);
+    const handlers = {
+      1: handleAckEvent,
+      2: handleReady,
+      3: handleGameCreate,
+      4: handleGameUpdate,
+      5: handleErrorEvent,
+    } as const;
+    console.log(data);
+    handlers[data.op](
+      ws,
+      data as any,
+      setToken,
+      setGameId,
+      setTurn,
+      board,
+      token
     );
   });
+  ws.send(
+    JSON.stringify({
+      op: 6,
+      d: {
+        type: "Identify",
+        username: "alaidriel",
+        password: "meow",
+      },
+    })
+  );
 
   return (
     <div class="flex flex-col min-h-screen items-center pt-5 bg-gray-800">
