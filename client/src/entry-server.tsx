0bff1524b3205285a08df98aa30bdabed3453657
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-06-09 17:38:09 -0500

    feat: set `theme-color` meta prop

--- client/src/entry-server.tsx
+++ client/src/entry-server.tsx
@@ -8,6 +8,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#8839ef" />
           {assets}
         </head>
         <body class="latte bg-base">
