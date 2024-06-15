4fee4b0e8b65e9f598bc41cbb3215d37d65cd6c8
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-06-15 14:57:41 -0500

    refactor: `strings::INVALID_USERNAME` is seen publicly

--- src/server/strings.rs
+++ src/server/strings.rs
@@ -3,13 +3,14 @@ pub const USERNAME_TOO_SHORT: &str = "Username must be at least 3 characters.";
 pub const PASSWORD_TOO_SHORT: &str = "Password must be at least 8 characters.";
 pub const PASSWORD_NO_ALPHA: &str = "Password must contain at least one alphabetic character.";
 pub const PASSWORD_NO_NUMERIC: &str = "Password must contain at least one number.";
+pub const INVALID_USERNAME: &str =
+    "That user doesn't exist! Make sure their username is spelled correctly.";
 
 // -- internal --
 pub const FRIEND_REQUEST_ALREADY_SENT: &str = "friend request already sent";
 pub const IDENTIFY_TIMEOUT: &str = "connection timed out";
 pub const INVALID_GAME_ID: &str = "no game exists with specified id";
 pub const INVALID_GAME_ID_FORMAT: &str = "invalid game id format (expected uuid)";
-pub const INVALID_USERNAME: &str = "no user exists with specified username";
 pub const USERNAME_TAKEN: &str = "that username is already taken";
 pub const INVALID_PASSWORD_FORMAT: &str = "password failed to hash correctly";
 pub const INVALID_PASSWORD: &str = "password does not match";
